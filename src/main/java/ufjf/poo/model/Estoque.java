345fe51daa6552615decb4f2520deab980b9e23b
Luan Brandão <luanbrandaodeoliveira@gmail.com>
2025-07-29 22:39:33 -0300

    classe Estoque criada

---
+++ src/main/java/ufjf/poo/model/Estoque.java
@@ -0,0 +1,17 @@
+package ufjf.poo.model;
+
+import java.util.List;
+
+public class Estoque {
+    
+    private List<ItemEstoque> produtos;
+    
+    public void adicionarProduto() {
+    }
+    
+    public void removerProduto() {
+    }
+    
+    public void listarProdutos() {
+    }
+}
