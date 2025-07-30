6be1c6182c3443e440cd0376ea3554a5d22b9f79
Luan Brandão <luanbrandaodeoliveira@gmail.com>
2025-07-29 22:40:07 -0300

    classe Unidade criada

---
+++ src/main/java/ufjf/poo/model/Unidade.java
@@ -0,0 +1,29 @@
+package ufjf.poo.model;
+
+import java.util.List;
+
+public class Unidade {
+    
+    private int id;
+    private String nome;
+    //private Endereco local;
+    private Gerente gerente;
+    private List<Vendedor> vendedores;
+    private Estoque estoque;
+    
+    public void adicionarVendedor() {
+    }
+    
+    public void removerVendedor() {
+    }
+    
+    public void gerarRelatorioVendas() {
+    }
+    
+    public void trocarGerente() {
+    }
+    
+    public List<Produto> listarEstoque() {
+        return null;
+    }
+}
