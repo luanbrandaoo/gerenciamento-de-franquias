3d017993fc415c3410e5649dfaeb24f163791191
Rafael Moreira <rafamuller05@gmail.com>
2025-07-30 17:56:08 -0300

    Estoque de produtos agora é um Map<Produto, Integer>, métodos criados e implementados Resolve conflitos de merge

--- src/main/java/ufjf/poo/model/estoque/Estoque.java
+++ src/main/java/ufjf/poo/model/estoque/Estoque.java
@@ -1,27 +1,61 @@
 package ufjf.poo.model.estoque;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.lang.Integer.max;
 import javax.xml.crypto.Data;
 
 public class Estoque {
     
-    private List<ItemEstoque> produtos;
-    
-    public Estoque(){
-        this.produtos = new ArrayList<>();
+    private Map<Produto, Integer> produtos;
+
+    public Estoque(Map<Produto, Integer> produtos) {
+        this.produtos = produtos;
+    }
+
+    public void adicionarProduto(Produto produto, Integer quantidade) {
+//        produtos.put(produto, produtos.getOrDefault(produto, 0) + quantidade);
+        produtos.compute(produto, (k, v) -> v == null ? quantidade : v + quantidade);
     }
     
-    public void adicionarProduto(Produto produto, int quantidade, Date validade) {
-        
-        ItemEstoque novoItem = new ItemEstoque(produto, quantidade, validade);
-        produtos.add(novoItem);
+    public void removerProduto(Produto produto) {
+        produtos.remove(produto);
     }
-    
-    public void removerProduto() {
+
+    public void subtraiProduto(Produto produto, Integer quantidade) {
+        produtos.compute(produto, (k, v) -> v == null ? 0 : max(v - quantidade, 0));
     }
+
+    public void atualizarProduto(Produto produto, Integer quantidade) {
+        produtos.put(produto, quantidade);
+    }
+
+    public int quantidadeProduto(Produto produto) {
+        return produtos.getOrDefault(produto, 0);
+    }
+
+    /*Necessário revisar implementação de ItemEstoque aqui*/
+//    public void adicionarItemEstoque(Produto produto, int quantidade, Date validade) {
+//        ItemEstoque novoItem = new ItemEstoque(produto, quantidade, validade);
+//        produtos.add(novoItem);
+//    }
     
-    public void listarProdutos() {
+    public Set<Produto> produtosEmEstoque() {
+        return produtos.keySet();
+    }
+
+    public Map<Produto, Integer> mapeiaEstoque() {
+        return produtos;
+    }
+
+    public void limparEstoque() {
+        produtos.clear();
+    }
+
+    public boolean produtoEmEstoque(Produto produto) {
+        return produtos.containsKey(produto) && produtos.get(produto) != 0;
     }
 }
