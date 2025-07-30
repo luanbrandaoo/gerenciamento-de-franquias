8a28246ae7fc99324fa88a1241f474915366c989
Luan Brandão <luanbrandaodeoliveira@gmail.com>
2025-07-29 22:41:06 -0300

    classe Session criada

---
+++ src/main/java/ufjf/poo/model/Session.java
@@ -0,0 +1,12 @@
+package ufjf.poo.model;
+
+public class Session {
+    
+    private Usuario usuarioLogado;
+    
+    public void login(Usuario usuario, String privateKey) {
+    }
+    
+    public void deslogar() {
+    }
+}
