1899c4b8f06d195e6b141d5ee34795605f7e23ce
Marcos Junior <mjjunior360@gmail.com>
2025-08-01 23:09:55 -0300

    Implementando funcoes de gerente e melhorando classe Unidade

--- src/main/java/ufjf/poo/model/Unidade.java
+++ src/main/java/ufjf/poo/model/Unidade.java
@@ -5,8 +5,10 @@ import ufjf.poo.model.estoque.Produto;
 import ufjf.poo.model.usuario.Gerente;
 import ufjf.poo.model.usuario.Vendedor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import ufjf.poo.model.pedido.Pedido;
 
 public class Unidade {
     
@@ -17,13 +19,13 @@ public class Unidade {
     private List<Vendedor> vendedores;
     private Estoque estoque;
 
-    public Unidade(int id, String nome, String endereco) {
+    public Unidade(int id, String nome, String endereco, Estoque estoque) {
         this.id = id;
         this.nome = nome;
         this.endereco = endereco;
         this.gerente = null;
-        this.vendedores = null;
-        this.estoque = null;
+        this.vendedores = new ArrayList<>();
+        this.estoque = estoque;
     }
     
     public void adicionarVendedor(Vendedor novoVendedor) {
@@ -36,9 +38,50 @@ public class Unidade {
     }
     
     public void gerarRelatorioVendas() {
+        if(vendedores.isEmpty()){
+            System.out.println("Nao ha Vendedores! ");
+            return;
+        }
+        
+        double totalVendasUnidade = 0.0;
+        int    totalPedidosUnidade = 0;
+        
+        System.out.println("Relatorio de vendas por vendedor: ");
+        
+        for(Vendedor vendedorAnalisado : vendedores){
+            double totalVendadsVendedor = 0.0;
+            
+            for(Pedido pedidoAnalisado : vendedorAnalisado.getPedidosRealizados()){
+                totalVendadsVendedor += pedidoAnalisado.getValorTotal();
+            }
+            
+            System.out.println("- Vendedor: " + vendedorAnalisado.getNome() + 
+                               vendedorAnalisado.getPedidosRealizados().size() + 
+                               " pedidos, Total R$" + String.format("%.2f", totalVendadsVendedor));
+            
+            totalPedidosUnidade += totalVendadsVendedor;
+            totalPedidosUnidade += vendedorAnalisado.getPedidosRealizados().size();
+        }
+        
+        System.out.println("Resumo da Unidade:");
+        System.out.println("Total de Pedidos: " + totalPedidosUnidade);
+        System.out.println("Total de Vendas: R$" + String.format("%.2f", totalVendasUnidade));
+        
     }
     
     public void trocarGerente(Gerente novoGerente) {
         gerente = novoGerente;
+    }
+    
+    public List<Vendedor> getVendedores(){
+        return vendedores;
+    }
+    
+    public Estoque getEstoque(){
+        return estoque;
+    }
+    
+    public String getNome(){
+        return nome;
     }
 }
--- src/main/java/ufjf/poo/model/usuario/Gerente.java
+++ src/main/java/ufjf/poo/model/usuario/Gerente.java
@@ -3,29 +3,85 @@ package ufjf.poo.model.usuario;
 import ufjf.poo.model.Unidade;
 
 import java.util.List;
+import ufjf.poo.model.estoque.Produto;
 
 public class Gerente extends Usuario {
     
     private Unidade unidadeFranquia;
-    private List<Vendedor> equipeDeVendedores;
+    //private List<Vendedor> equipeDeVendedores;
     
-    public Gerente(String nome, int id, String key, String email) {
+    public Gerente(String nome, int id, String key, String email,Unidade unidadeFranquia){
         super(nome, id, key, email);
+        this.unidadeFranquia = unidadeFranquia;
     }
 
     public void gerenciaEquipes() {
-
+       
+        System.out.println("\n--- Gerenciamento de Equipe ---");
+        List<Vendedor> vendedores = unidadeFranquia.getVendedores();
+        
+        if(vendedores.isEmpty() || vendedores == null){
+            System.out.println("A unidade não possui vendedores.");
+            return;
+        }
+        
+        System.out.println("Vendedores da unidade " + unidadeFranquia.getNome());
+        
+        for(Vendedor v : vendedores){
+            System.out.println("- ID: " + v.getId() + " Nome: " + v.getNome() + " E-mail" + v.getEmail());
+            System.out.println(" Pedidos realizados: " + v.getPedidosRealizados().size());
+        }
+        
     }
     
     public void controlaPedidos() {
-
+        
+        System.out.println("\n--- Controle de Pedidos da Unidade ---");
+        List<Vendedor> vendedores = unidadeFranquia.getVendedores();
+        
+        if(vendedores.isEmpty()){
+            System.out.println("Nenhum vendedor Cadastrado! ");
+            return;
+        }
+        
+        for(Vendedor v :  vendedores){
+            System.out.println("\nPedidos do Vendedor: " + v.getNome());
+            v.visualizarPedidos();
+        }
+        
     }
     
-    public void administrarEstoque() {
+    public void administrarEstoque(Produto produto, int quantidade , String acao) {
+        
+        if (unidadeFranquia.getEstoque() == null) {
+            System.out.println("Estoque não configurado para a unidade.");
+            return;
+        }
+        
+        System.out.println("\n--- Administração de Estoque ---");
+        
+        if (acao.equalsIgnoreCase("adicionar")) {
+            unidadeFranquia.getEstoque().adicionarProduto(produto, quantidade);
+            System.out.println("Produto " + produto.getNome() + " adicionado com sucesso.");
+        } 
+     
+        else if (acao.equalsIgnoreCase("remover")) {
+            unidadeFranquia.getEstoque().removerProduto(produto);
+            System.out.println("Produto " + produto.getNome() + " removido com sucesso.");
+        }
+        
+        else if (acao.equalsIgnoreCase("listar")) {
+            unidadeFranquia.getEstoque().listarProdutos();    
+        }
+        
+        else {
+            System.out.println("Ação '" + acao + "' não reconhecida.");
+        }
 
     }
     
     public void relatorioDeDesempenho() {
-
+        System.out.println("Relatorio da Unidade: ");
+        unidadeFranquia.gerarRelatorioVendas();
     }
 }
